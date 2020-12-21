ff9e159af3e7a35f35985b28e090687f31c0188a
sharanmsd <shar17220.it@rmkec.ac.in>
2020-12-21 11:42:34 +0530

    MYFIRSTCOMMIT

---
+++ test.py
@@ -0,0 +1,5 @@
+import webapp2
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Hello World App Executed Successfully")
+app=webapp2.WSGIApplication([('/', MainPage),],debug=True)
